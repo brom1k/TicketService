251d01defcccb6ab3ebcc31fda9029e594b1c50f
brom1k <36930918+brom1k@users.noreply.github.com>
2018-03-05 11:50:07 +0200

    first commit

---
+++ app/src/main/java/com/ticketservice/ticketservice/presentation/event/view/EventListActivity.java
@@ -0,0 +1,15 @@
+package com.ticketservice.ticketservice.presentation.event.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ticketservice.ticketservice.R;
+
+public class EventListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_list);
+    }
+}
